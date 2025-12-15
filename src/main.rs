2a166e0474b17609a79dfe23cfa2a23ddc31b335
Yh <Yh@qq.com>
2025-12-15 09:40:15 +0800

    feat: 设置默认日志级别为info

--- src/main.rs
+++ src/main.rs
@@ -180,7 +180,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // 初始化日志系统
     tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_env_filter(tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()))
         .init();
 
     info!(
